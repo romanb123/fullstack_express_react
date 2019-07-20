0e13071bb4fdd1e6bb1477684d8c194d43c17387
romanb123 <44100462+romanb123@users.noreply.github.com>
2019-07-20 18:59:24 +0300

    fghfgh

--- backend/myapp/routes/users.js
+++ backend/myapp/routes/users.js
@@ -1,26 +1,27 @@
 var express = require('express');
 var router = express.Router();
-router.use(function(req, res, next) {
+var mysql = require('mysql');
+const pool = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  database: 'node-app',
+  password: 'qwert%$#@!'
+});
+router.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
 /* GET users listing. */
-router.get('/', function (req, res) {
-  var data = {
-    "bestAnimals": [
-      "wombat",
-      "corgi",
-      "puffer fish",
-      "owl",
-      "crow"
-    ]
-  };
-
-  res.json(data);
+router.get("/", function (req, res) {
+  pool.query("SELECT * FROM products", function (err, data) {
+    if (err) return console.log(err);
+    res.send(data);
+  });
 });
-router.post('/', function (req, res){
-console.log(req.body);
+
+router.post('/', function (req, res) {
+  console.log(req.body);
 });
 module.exports = router;
